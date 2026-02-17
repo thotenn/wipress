350f579c21fde62bf746b3536740a009500839e3
Jose Garay <jose.garay@fortyau.com>
2026-02-17 20:19:11 -0300

    ui fix

--- templates/single-wiki.php
+++ templates/single-wiki.php
@@ -1,6 +1,6 @@
 <?php
 ?><!DOCTYPE html>
-<html <?php language_attributes(); ?>>
+<html <?php language_attributes(); ?> class="wipress-page">
 <head>
     <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1">
